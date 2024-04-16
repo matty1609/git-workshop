ed4ee7f6c7ada97b4a41f13375ed354d6a54d14e
Katerina Pilatova <katerina.pilatova@flowup.cz>
2024-04-16 15:34:58 +0200

    Add multiply method

--- src/index.test.ts
+++ src/index.test.ts
@@ -1,7 +1,13 @@
-import { sum } from "./index";
+import { multiply, sum } from "./index";
 
 describe("sum", () => {
   test("should add up multiple numbers", () => {
     expect(sum([1, 2, 3, 4])).toBe(10);
+  });
+});
+
+describe("multiply", () => {
+  test("should multiply several numbers", () => {
+    expect(multiply([2, 3, 4])).toBe(24);
   });
 });
--- src/index.ts
+++ src/index.ts
@@ -2,4 +2,9 @@ export function sum(numbers: number[]): number {
   return numbers.reduce((acc, value) => acc + value, 0);
 }
 
+export function multiply(numbers: number[]): number {
+  return numbers.reduce((acc, value) => acc * value, 1);
+}
+
 console.log(sum([1, 2, 3, 4, 5]));
+console.log(multiply([2, 3, 4]));
