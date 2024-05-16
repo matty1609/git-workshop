f5688b21fd27103572d19ec124b7043d4a00e503
Katerina Pilatova <katerina.pilatova@flowup.cz>
2024-05-16 15:17:43 +0200

    Distinguish node execution flow for coverage

--- src/index.ts
+++ src/index.ts
@@ -2,18 +2,21 @@ export function sum(numbers: number[]): number {
   return numbers.reduce((acc, value) => acc + value, 0);
 }
 
-const MONDAY_COUNT = 1;
-const TUESDAY_COUNT = 2;
-const WEDNESDAY_COUNT = 3;
-const THURSDAY_COUNT = 4;
-const FRIDAY_COUNT = 5;
+// eslint-disable-next-line unicorn/prefer-module
+if (require.main === module) {
+  const MONDAY_COUNT = 1;
+  const TUESDAY_COUNT = 2;
+  const WEDNESDAY_COUNT = 3;
+  const THURSDAY_COUNT = 4;
+  const FRIDAY_COUNT = 5;
 
-console.info(
-  sum([
-    MONDAY_COUNT,
-    TUESDAY_COUNT,
-    WEDNESDAY_COUNT,
-    THURSDAY_COUNT,
-    FRIDAY_COUNT,
-  ]),
-);
+  console.info(
+    sum([
+      MONDAY_COUNT,
+      TUESDAY_COUNT,
+      WEDNESDAY_COUNT,
+      THURSDAY_COUNT,
+      FRIDAY_COUNT,
+    ]),
+  );
+}
